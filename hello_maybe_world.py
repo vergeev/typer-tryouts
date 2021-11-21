184e9ba608a83dc549f709d67340473e8106e0cd
Pavel Vergeev <vergeev@hey.com>
2021-11-21 18:45:58 +0300

    add a renamed argument example

--- hello_maybe_world.py
+++ hello_maybe_world.py
@@ -7,7 +7,11 @@ def generate_random_name() -> str:
     return random.choice(["World", "Cruel World", "Mate", "Ma Man"])
 
 
-def main(name: str = typer.Argument(generate_random_name, help="Who to greet", show_default=False)) -> None:
+def main(
+    name: str = typer.Argument(
+        generate_random_name, metavar="name🧚", help="Who to greet", show_default=False
+    )
+) -> None:
     typer.echo(f"Hello {name}!")
 
 
