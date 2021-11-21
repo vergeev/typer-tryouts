1a94229040ba1283b2149870e340a52ebbf3d4d6
Pavel Vergeev <vergeev@hey.com>
2021-11-21 18:13:57 +0300

    add normal early exit example

---
+++ maybe_create_user.py
@@ -0,0 +1,26 @@
+import typer
+
+
+existing_usernames = ["john doe", "jane doe"]
+
+
+def maybe_create_user(username: str) -> None:
+    if username in existing_usernames:
+        typer.echo("The user already exists")
+        raise typer.Exit()
+    else:
+        existing_usernames.append(username)
+        typer.echo(f"User created: {username}")
+
+
+def send_new_notification(username: str) -> None:
+    typer.echo(f"Notification sent to the new user: {username}")
+
+
+def main(username: str) -> None:
+    maybe_create_user(username)
+    send_new_notification(username)
+
+
+if __name__ == "__main__":
+    typer.run(main)
